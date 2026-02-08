8f8bf1424222dcd39a8e3f5838f838380f5dc2aa
Anurg29 <anuragrokade9@gmail.com>
2026-02-09 01:59:21 +0530

    feat: Auto-create admin user during deployment

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create admin user (won't fail if already exists)
+python manage.py create_admin
